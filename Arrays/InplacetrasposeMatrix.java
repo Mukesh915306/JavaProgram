41cc66fffa7cd6f8c4e1e5e071aa5b13a274c6b2
Mukesh Kumar <155368282+Mukesh915306@users.noreply.github.com>
2025-10-29 21:53:24 +0530

    Add in-place matrix transpose implementation

---
+++ Arrays/InplacetrasposeMatrix.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Inplacetranspose {
+    public static void inPlaceTranspose(int[][] arr, int r, int c) {
+        for (int i = 0; i < c; i++) {
+            for (int j = i; j < r; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int[][] arr = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        inPlaceTranspose(arr, r, c);
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+
+}
